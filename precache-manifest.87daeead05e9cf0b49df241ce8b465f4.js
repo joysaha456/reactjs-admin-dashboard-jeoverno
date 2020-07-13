7ae83b23a2887433da21ea191d9fec84fdec5329
Joy Saha <joysaha544@gmail.com>
2020-07-13 18:39:02 +0600

    Updates

---
+++ precache-manifest.87daeead05e9cf0b49df241ce8b465f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bec544909ed25e87dbbeb20d6ae3e6",
+    "url": "/reactjs-admin-dashboard-jeoverno/index.html"
+  },
+  {
+    "revision": "b9aebe006f0f495d9415",
+    "url": "/reactjs-admin-dashboard-jeoverno/static/css/2.ecd3226c.chunk.css"
+  },
+  {
+    "revision": "7a762b1de9d5b3534c6e",
+    "url": "/reactjs-admin-dashboard-jeoverno/static/css/main.c8c89e32.chunk.css"
+  },
+  {
+    "revision": "b9aebe006f0f495d9415",
+    "url": "/reactjs-admin-dashboard-jeoverno/static/js/2.f37d7988.chunk.js"
+  },
+  {
+    "revision": "ec54c5b71ce1878721d292acd7d736b6",
+    "url": "/reactjs-admin-dashboard-jeoverno/static/js/2.f37d7988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a762b1de9d5b3534c6e",
+    "url": "/reactjs-admin-dashboard-jeoverno/static/js/main.14facb70.chunk.js"
+  },
+  {
+    "revision": "24f0b6b89dfcb17e85ff",
+    "url": "/reactjs-admin-dashboard-jeoverno/static/js/runtime-main.8ab8ab9d.js"
+  }
+]);
